ff809ce96f979c0f2a1d4989ce8fbe254a1e7ec7
Fadi <fadi.al-salti@capevision.de>
2022-07-15 11:54:50 +0200

    added header and footer

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes'
+import { DishService } from '../services/dish.service';
  
 @Component({
   selector: 'app-menu',
@@ -11,12 +12,14 @@ import { DISHES } from '../shared/dishes'
 export class MenuComponent implements OnInit {
 
   dishes = DISHES;
+  //dishes: Dish[] Why it is not working?
 
   selectedDish: Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
+    this.dishes = this.dishService.getDishes();
   }
 
   onSelect(dish: Dish)  {
