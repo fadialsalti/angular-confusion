c06bb83bec45c6eb39fd384947c87ea5ce443566
Fadi <fadi.al-salti@capevision.de>
2022-07-15 20:47:19 +0200

    routed to different dishes

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes'
+//import { DISHES } from '../shared/dishes'
 import { DishService } from '../services/dish.service';
  
 @Component({
@@ -11,8 +11,8 @@ import { DishService } from '../services/dish.service';
 
 export class MenuComponent implements OnInit {
 
-  dishes = DISHES;
-  //dishes: Dish[] Why it is not working?
+  //dishes = DISHES;
+  dishes: Dish[];
 
   selectedDish: Dish;
 
